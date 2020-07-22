3841b0a7edbf274dfa4492d0ed05951bb14c1be1
Obi-Two <johnroyuk@gmail.com>
2020-07-22 21:59:54 +0100

    init commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SWGANH_WorldServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
